673b19c49109d4f7cd50a4cdb1e93c870947a6dd
Zhuoda Li <zhuodali028@gmail.com>
2026-09-20 21:27:45 +0000

    third

--- my_notebook/docker_command.sql
+++ my_notebook/docker_command.sql
@@ -1,3 +1,12 @@
 -- CREATE Images containers ANYTIME AND USE THEM 
--- CREATE Images
--- 
+-- CREATE Images after [editing the Dockerfile]
+cd image_dir
+docker build -t image_name:v1 .
+-- Create different containers, then start, interact/quit, Stop it
+docker create --name my_container nginx 
+docker create --name my_container ubuntu:20.04
+docker start container1 container2 container3
+docker stop container1
+docker exec -it 4c0253d0cb72 bash
+
+
